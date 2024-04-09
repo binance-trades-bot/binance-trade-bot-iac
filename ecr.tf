08c3d290fe6f4288cd81f6f3d77f3e155fa4445e
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-04-08 21:15:52 -0300

    feat: add kms policy

--- ecr.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "ecr_repository_binance_trades" {
-  name         = "binance-trades-bot"
-  force_delete = true
-}
--- iam.tf
+++ iam.tf
@@ -15,3 +15,25 @@ resource "aws_iam_role" "role_binance_trades_bot" {
   name               = "role_binance_trades_bot"
   assume_role_policy = data.aws_iam_policy_document.policy_lambda.json
 }
+
+resource "aws_iam_policy" "policy_kms" {
+  name        = "binance_trades_access_kms"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "kms:Decrypt",
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:kms:*:255310614335:key/*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_kms" {
+  role       = aws_iam_role.role_binance_trades_bot.name
+  policy_arn = aws_iam_policy.policy_kms.arn
+}
