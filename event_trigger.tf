fc53e8274620a82823aa07c571f8dd6e0f232d44
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-04-11 19:42:17 -0300

    feat: configura event trigger

---
+++ event_trigger.tf
@@ -0,0 +1,9 @@
+resource "aws_cloudwatch_event_rule" "lambda_trigger" {
+  name = "binance-trade-event"
+  schedule_expression = "cron(0/1 * ? * * *)"
+}
+
+resource "aws_cloudwatch_event_target" "sns" {
+  rule      = aws_cloudwatch_event_rule.lambda_trigger.name
+  arn       = aws_lambda_function.lambda_function.arn
+}
